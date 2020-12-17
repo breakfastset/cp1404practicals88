3e9d8355eab950f398ae81b2d559894604ebf728
kangleng <kangleng@gmail.com>
2020-12-17 13:53:10 +0800

    Add new file

---
+++ prac_03/another_file.py
@@ -0,0 +1,3 @@
+print("This is just an example")
+
+print("To show that it goes to the develop branch")
--- prac_03/my_prac03_file.py
+++ prac_03/my_prac03_file.py
@@ -1,8 +1,10 @@
 print("Test branch")
-
 for i in range(3):
     print(i)
-
 print('Hurray')
-
 print('wow')
+print("This is extra statement")
+print("This is another one")
+
+for i in range(10):
+    print(i+1)
